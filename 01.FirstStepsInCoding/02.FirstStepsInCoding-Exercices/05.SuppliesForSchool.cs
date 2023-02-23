38dab251218bbc739a493b9e5c07416b60d2caff
Radoslav Mihaylov <124840636+RadoMihaylov@users.noreply.github.com>
2023-02-23 23:59:26 +0200

    Add files via upload

---
+++ 01.FirstStepsInCoding/02.FirstStepsInCoding-Exercices/05.SuppliesForSchool.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace _05.SuppliesForSchool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int pens = int.Parse(Console.ReadLine());
+            int markers = int.Parse(Console.ReadLine());
+            int litersOfDetergent = int.Parse(Console.ReadLine());
+            int discount = int.Parse(Console.ReadLine());
+
+            double sum = pens * 5.8 + markers * 7.2 + litersOfDetergent * 1.2;
+            double finalPrice = sum - (sum * discount * 0.01);
+            Console.WriteLine(finalPrice);
+
+        }
+    }
+}
