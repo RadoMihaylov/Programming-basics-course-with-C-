a0bf3f408311e4b46c6f52afcb8caca58ec27b43
Radoslav Mihaylov <124840636+RadoMihaylov@users.noreply.github.com>
2023-02-24 00:00:12 +0200

    Add files via upload

---
+++ 01.FirstStepsInCoding/02.FirstStepsInCoding-Exercices/07.FoodDelivery.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace _07.FoodDelivery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countChickenMenu = int.Parse(Console.ReadLine());
+            int fishMenu = int.Parse(Console.ReadLine());
+            int vegetarianMenu = int.Parse(Console.ReadLine());
+
+            double sum = countChickenMenu * 10.35 + fishMenu * 12.4 + vegetarianMenu * 8.15;
+            double finalPrice = sum + (sum * 0.2) + 2.5;
+            Console.WriteLine(finalPrice);
+
+        }
+    }
+}
