a3cf45e1ea1524d24e7143c5bc88d7a53bb0d91b
Radoslav Mihaylov <124840636+RadoMihaylov@users.noreply.github.com>
2023-02-25 23:52:53 +0200

    Add files via upload

---
+++ 01.FirstStepsInCoding/02.FirstStepsInCoding-Exercices/07.Repainting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _07.Repainting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nylon = int.Parse(Console.ReadLine());
+            int paint = int.Parse(Console.ReadLine());
+            int diluent = int.Parse(Console.ReadLine());
+            int workHours = int.Parse(Console.ReadLine());
+
+            double sum = (nylon + 2) * 1.5 + (paint + paint * 0.1) * 14.5 + (diluent * 5.0) + 0.4;
+            double workersSum = workHours * (sum * 0.3);
+            double finalPrice = workersSum + sum;
+            Console.WriteLine(finalPrice);
+        }
+    }
+}
