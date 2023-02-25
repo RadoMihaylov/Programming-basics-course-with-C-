dbf6f67cc5b79a8a9ed392dcfef31a5b56a8be21
Radoslav Mihaylov <124840636+RadoMihaylov@users.noreply.github.com>
2023-02-25 23:53:55 +0200

    Add files via upload

---
+++ 01.FirstStepsInCoding/02.FirstStepsInCoding-Exercices/08.BasketballEquipment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _08.BasketballEquipment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int annualTax = int.Parse(Console.ReadLine());
+
+            double sneakers = annualTax * 0.6;
+            double equip = sneakers * 0.8;
+            double ball = equip * 0.25;
+            double accessoires = ball * 0.2;
+
+            double expense = sneakers + equip + accessoires + ball + annualTax;
+            Console.WriteLine(expense);
+
+        }
+    }
+}
---
+++ 01.FirstStepsInCoding/02.FirstStepsInCoding-Exercices/09.FishTank.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _09.FishTank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lenght = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());  
+            int height = int.Parse(Console.ReadLine());
+            double pourcentage = double.Parse(Console.ReadLine()) / 100;
+
+            double volume = (lenght * width * height) * 0.001;
+            double finalVolume = volume - (volume * pourcentage);
+
+            Console.WriteLine(finalVolume);
+            
+        }
+    }
+}
