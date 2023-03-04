e9997f1ff5e3fb1a3cbce14ace4bb11cf100b6cd
Radoslav Mihaylov <124840636+RadoMihaylov@users.noreply.github.com>
2023-03-04 23:57:30 +0200

    Add files via upload

---
+++ 01.FirstStepsInCoding/03.ConditionalStatements-Lab/07.AreaOfFigures.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Drawing;
+using System.Transactions;
+
+namespace _07.AreaOfFigures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string figure = Console.ReadLine();
+           
+            if (figure == "square")
+            {
+                double lenghtA = double.Parse(Console.ReadLine());
+                double area = lenghtA * lenghtA;
+                Console.WriteLine($"{area:f3}");
+            }
+            else if (figure == "rectangle")
+            {
+                double lenghtA = double.Parse(Console.ReadLine());
+                double lenghtB = double.Parse(Console.ReadLine());
+                double area = lenghtA * lenghtB;
+                Console.WriteLine($"{area:f3}");
+            }
+            else if (figure == "circle")
+            {
+                double r = double.Parse(Console.ReadLine());
+                double area = Math.Pow(r, 2) * Math.PI;
+                Console.WriteLine($"{area:f3}");
+
+            }
+            else if (figure == "triangle")
+            {
+                double lenghtA = double.Parse(Console.ReadLine());
+                double heightB = double.Parse(Console.ReadLine());
+                double area = (lenghtA * heightB) / 2;
+                Console.WriteLine($"{area:f3}");
+
+            }
+        
+        }
+    }
+}
