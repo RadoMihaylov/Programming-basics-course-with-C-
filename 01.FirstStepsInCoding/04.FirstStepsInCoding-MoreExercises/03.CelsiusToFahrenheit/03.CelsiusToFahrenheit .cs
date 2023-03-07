559e16aac5ba50c52280b656a23bb5b5cea980f8
Radoslav Mihaylov <124840636+RadoMihaylov@users.noreply.github.com>
2023-03-08 00:01:27 +0200

    Add task: CelsiusToFahrenheit

---
+++ 01.FirstStepsInCoding/04.FirstStepsInCoding-MoreExercises/03.CelsiusToFahrenheit/03.CelsiusToFahrenheit .cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _03.CelsiusToFahrenheit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double degreeCelsius = double.Parse(Console.ReadLine());
+            // * 1.8 + 32 
+            double degreeFahrenheit = degreeCelsius * 1.8 + 32;
+            Console.WriteLine($"{degreeFahrenheit:f2}");
+
+
+
+        }
+    }
+}
