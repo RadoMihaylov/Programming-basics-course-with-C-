fe638ac10a6112f9fdf6115c3913be991affeead
Radoslav Mihaylov <124840636+RadoMihaylov@users.noreply.github.com>
2023-03-11 00:05:42 +0200

    Add task: TrainingLab

---
+++ 01.FirstStepsInCoding/04.FirstStepsInCoding-MoreExercises/05.TrainingLab/05.TrainingLab.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _05.TrainingLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //От конзолата се четат 2 числа, по едно на ред: w(дължина в метри) и h(широчина в метри).
+            //Ограничения: 3 ≤ h ≤ w ≤ 100.
+
+            double lenhgtW = double.Parse(Console.ReadLine());
+            double heightH = double.Parse(Console.ReadLine());
+            Console.WriteLine(heightH * lenhgtW);
+
+
+        }
+    }
+}
