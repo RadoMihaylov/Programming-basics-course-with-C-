ee316a4d8f1f62edf8f6152be08fd1ecb6b8363a
Radoslav Mihaylov <124840636+RadoMihaylov@users.noreply.github.com>
2023-03-11 01:40:37 +0200

    Add task: TrapeziodArea

---
+++ 01.FirstStepsInCoding/04.FirstStepsInCoding-MoreExercises/01.TrapeziodArea/01.TrapeziodArea .cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace _01.TrapeziodArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double sideB1 = double.Parse(Console.ReadLine());
+            double sideB2 = double.Parse(Console.ReadLine());
+            double heightH = double.Parse(Console.ReadLine());
+
+            double trapezArea = (sideB1 + sideB2) * heightH / 2;
+            Console.WriteLine($"{trapezArea:f2}");
+
+
+        }
+    }
+}
