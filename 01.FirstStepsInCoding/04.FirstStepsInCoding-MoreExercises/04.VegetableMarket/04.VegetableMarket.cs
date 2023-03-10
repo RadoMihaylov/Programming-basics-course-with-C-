cc1b8a3637a63523f7b2bcd738298099dbdc3332
Radoslav Mihaylov <124840636+RadoMihaylov@users.noreply.github.com>
2023-03-11 00:00:19 +0200

    Add task: 04.VegetableMarket

---
+++ 01.FirstStepsInCoding/04.FirstStepsInCoding-MoreExercises/04.VegetableMarket/04.VegetableMarket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _04.VegetableMarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double priceKgVegetables = double.Parse(Console.ReadLine());
+            double priceKgFruits = double.Parse(Console.ReadLine());
+            int totalKgVegetables = int.Parse(Console.ReadLine());
+            int totalKgFruits = int.Parse(Console.ReadLine());
+
+
+            double totalSum = (priceKgFruits * totalKgFruits) + (priceKgVegetables * totalKgVegetables);
+             double Sum = totalSum / 1.94;
+
+                
+            Console.WriteLine($"{Sum:f2}");
+         }
+    }
+}
