a87d327da992550ee3a1734d610a4fa308019d85
Radoslav Mihaylov <124840636+RadoMihaylov@users.noreply.github.com>
2023-03-17 00:13:21 +0200

    Add task: Personal Titles

---
+++ 01.FirstStepsInCoding/06.ConditionalStatementsAdvanced/01.ConditionalStatementsAdvanced-Lab/04.PersonalTitles/04.PersonalTitles .cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _04.PersonalTitles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double age = double.Parse(Console.ReadLine());
+            string gender = Console.ReadLine();
+
+            if (gender == "f")
+            {
+                if (age >= 16)
+                {
+                    Console.WriteLine("Ms.");
+                }
+                else if (age < 16)
+                {
+                    Console.WriteLine("Miss");
+                }
+            }
+            if (gender == "m")
+            {
+                if (age >= 16)
+                {
+                    Console.WriteLine("Mr.");
+                }
+                else if (age < 16)
+                {
+                    Console.WriteLine("Master");
+                }
+            }
+
+        }
+
+        }   
+    }
+}
