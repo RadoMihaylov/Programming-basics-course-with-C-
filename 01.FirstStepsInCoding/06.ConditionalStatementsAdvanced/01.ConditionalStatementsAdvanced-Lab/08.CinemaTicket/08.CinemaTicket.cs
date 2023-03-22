7d012a30de0c937093d37ac787233997b7c546a5
Radoslav Mihaylov <124840636+RadoMihaylov@users.noreply.github.com>
2023-03-22 23:34:56 +0200

    Update 08.CinemaTicket.cs

--- 01.FirstStepsInCoding/06.ConditionalStatementsAdvanced/01.ConditionalStatementsAdvanced-Lab/08.CinemaTicket/08.CinemaTicket.cs
+++ 01.FirstStepsInCoding/06.ConditionalStatementsAdvanced/01.ConditionalStatementsAdvanced-Lab/08.CinemaTicket/08.CinemaTicket.cs
@@ -1,1 +1,28 @@
+using System;
 
+namespace _08.CinemaTicket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             //Monday  Tuesday  Wednesday  Thursday    Friday  Saturday    Sunday
+             // 12       12        14        14          12      16         16
+
+            string day = Console.ReadLine();
+
+            if (day == "Monday" || day == "Tuesday" || day == "Friday")
+            {
+                Console.WriteLine(12);
+            }
+            else if (day == "Wednesday" || day == "Thursday")
+            {
+                Console.WriteLine(14);
+            }
+            else if (day == "Saturday" || day == "Sunday")
+            {
+                Console.WriteLine(16);
+            }
+        }
+    }
+}
