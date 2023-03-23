4db67dc981b14cbe9612e764329179567cd5658f
Radoslav Mihaylov <124840636+RadoMihaylov@users.noreply.github.com>
2023-03-23 22:55:37 +0200

    Update 09.FruitOrVegetable .cs

--- 01.FirstStepsInCoding/06.ConditionalStatementsAdvanced/01.ConditionalStatementsAdvanced-Lab/09.FruitOrVegetable/09.FruitOrVegetable .cs
+++ 01.FirstStepsInCoding/06.ConditionalStatementsAdvanced/01.ConditionalStatementsAdvanced-Lab/09.FruitOrVegetable/09.FruitOrVegetable .cs
@@ -1,1 +1,36 @@
+using System;
 
+namespace _09.FruitOrVegetable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+           string nameProduct = Console.ReadLine();
+
+            switch (nameProduct)
+            {
+                case "banana":
+                case "apple":
+                case "kiwi":
+                case "cherry":
+                case "lemon":
+                case "grapes":
+                    Console.WriteLine("fruit");
+                    break;
+                case "tomato":
+                case "cucumber":
+                case "pepper":
+                case "carrot":             
+                    Console.WriteLine("vegetable");
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+
+        }
+    }
+}
