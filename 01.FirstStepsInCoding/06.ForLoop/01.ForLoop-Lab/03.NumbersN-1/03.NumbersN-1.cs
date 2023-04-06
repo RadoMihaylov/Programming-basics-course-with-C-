0ccf082623873f854f6657c051b72aa6a2f2d394
Radoslav Mihaylov <124840636+RadoMihaylov@users.noreply.github.com>
2023-04-06 23:41:23 +0300

    Add task: Numbers N-1

---
+++ 01.FirstStepsInCoding/06.ForLoop/01.ForLoop-Lab/03.NumbersN-1/03.NumbersN-1.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace _02.NumbersN_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = num; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
